a8320c31922dcbda5177b7047a893221531c31b9
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 23:16:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,7 +13,7 @@ def reverse_each_word(string)
   new_array = []
   parts = string.split(" ")
   reverse =  parts.collect { |part| part.reverse }.join(" ")
-  ba
+  binding.pry
 end
 
 reverse_each_word("Hello there, and how are you?")
