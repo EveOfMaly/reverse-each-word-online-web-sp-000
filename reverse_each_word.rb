d8716820297e16f248ecefb724198c83a727b710
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 23:17:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
-def reverse_each_word(string)
-  new_array = []
-  parts = string.split(" ")
-  parts.each do |part|
-    new_array.push(part.reverse) 
-  end
-  new_array.join(" ")
-end
+# def reverse_each_word(string)
+#   new_array = []
+#   parts = string.split(" ")
+#   parts.each do |part|
+#     new_array.push(part.reverse) 
+#   end
+#   new_array.join(" ")
+# end
 
 def reverse_each_word(string)
   new_array = []
