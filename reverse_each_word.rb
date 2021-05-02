cea1ffd962ffc4c9aec8daa1bd254d3978c50687
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 23:08:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   new_string = ""
   parts = string.split(" ")
   parts.each do |part|
-    new_string << part
+    new_string << part.reverse 
   end
   p new_string
 end
