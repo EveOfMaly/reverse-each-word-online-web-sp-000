383f37048ed8b86845b80eece3909e7accb24c6c
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 23:13:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,19 @@
 require 'pry'
 
-# def reverse_each_word(string)
-#   new_array = []
-#   parts = string.split(" ")
-#   parts.each do |part|
-#     new_array.push(part.reverse) 
-#   end
-#   p new_array.join(" ")
-# end
+def reverse_each_word(string)
+  new_array = []
+  parts = string.split(" ")
+  parts.each do |part|
+    new_array.push(part.reverse) 
+  end
+  new_array.join(" ")
+end
 
 def reverse_each_word(string)
   new_array = []
   parts = string.split(" ")
   reverse =  parts.map { |part| part.reverse }
-  p reverse
+  reverse
 end
 
 reverse_each_word("Hello there, and how are you?")
